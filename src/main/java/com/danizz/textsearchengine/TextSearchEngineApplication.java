50eff179be198c78b09466382d17badcf78e07d4
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-05-22 01:08:57 +0300

    Initial commit

---
+++ src/main/java/com/danizz/textsearchengine/TextSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.danizz.textsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSearchEngineApplication.class, args);
+	}
+
+}
