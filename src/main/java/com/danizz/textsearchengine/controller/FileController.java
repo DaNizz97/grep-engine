69b0fd40a9d0cf84d260b2daaa3c85b9620e08da
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-05-24 01:08:32 +0300

    Implement ability to read large files anf write it to client Create class that allows to read files in back order

--- src/main/java/com/danizz/textsearchengine/controller/FileController.java
+++ src/main/java/com/danizz/textsearchengine/controller/FileController.java
@@ -1,13 +1,16 @@
 package com.danizz.textsearchengine.controller;
 
+import com.danizz.textsearchengine.ReverseLineReader;
 import com.danizz.textsearchengine.dto.Node;
 import com.danizz.textsearchengine.dto.SearchFileRequest;
 import com.danizz.textsearchengine.dto.SearchTextRequest;
 import com.danizz.textsearchengine.service.FileSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,9 +36,34 @@ public class FileController {
     }
 
     @PostMapping("/search")
-    public List<String> findByFile(@RequestBody SearchTextRequest request) throws IOException {
-        return Files.lines(Paths.get(request.getFileName()))
-                .filter(s -> s.contains(request.getPattern()))
-                .collect(Collectors.toList());
+    public StreamingResponseBody findByFile(@RequestBody SearchTextRequest request) {
+        //TODO: Move method implementation into service
+        return out -> {
+            ReverseLineReader reader = new ReverseLineReader(new File(request.getFileName()));
+            String line;
+            int i = 0;
+            while ((line = reader.readLine()) != null && i < request.getMaxLinesCount()) {
+                if (line.contains(request.getPattern())) {
+                    out.write((line + "\n").getBytes());
+                    out.flush();
+                    i++;
+                }
+            }
+        };
     }
+
+//    @GetMapping("/test")
+//    public StreamingResponseBody handleRequest() {
+//        return out -> {
+//            ReverseLineReader reader = new ReverseLineReader(new File("/home/da-nizz/IdeaProjects/textsearchengine/src/main/java/com/danizz/file.txt"));
+//            String line;
+//            int i = 0;
+//            while ((line = reader.readLine()) != null) {
+//                if (line.contains("v")) {
+//                    out.write((line + "\n").getBytes());
+//                    out.flush();
+//                }
+//            }
+//        };
+//    }
 }
--- src/main/java/com/danizz/textsearchengine/dto/SearchTextRequest.java
+++ src/main/java/com/danizz/textsearchengine/dto/SearchTextRequest.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class SearchTextRequest {
     private String fileName;
     private String pattern;
+    private int maxLinesCount;
+    private boolean revert;
 }
