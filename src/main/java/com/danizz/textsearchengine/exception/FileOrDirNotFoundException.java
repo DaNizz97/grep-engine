af3aa6a772f861d51cd67c78446bebb3a64b82a6
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-06-07 22:21:00 +0300

    Change not found exception status to 404

--- src/main/java/com/danizz/textsearchengine/exception/FileOrDirNotFoundException.java
+++ src/main/java/com/danizz/textsearchengine/exception/FileOrDirNotFoundException.java
@@ -4,7 +4,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NO_CONTENT)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 @NoArgsConstructor
 public class FileOrDirNotFoundException extends RuntimeException {
 
